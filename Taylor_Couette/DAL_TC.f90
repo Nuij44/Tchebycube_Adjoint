31bea800c3d5b3d856eb0bd31fcb071374b5c20c
Nuij44 <clement.mariot@gmail.com>
2026-02-27 16:43:42 +0100

    fix dealiasing

--- Taylor_Couette/DAL_TC.f90
+++ Taylor_Couette/DAL_TC.f90
@@ -268,6 +268,8 @@ program tcheby_1d
 
   CALL DUMP_HDF5_BASIC(FILE_DUMP,'NEW',TC,DT,MSH,UA,UZ,UR,PRES,DG01,DG09)
 
+  CALL DEALIAZING(UA,UZ,UR)
+  
   DG10 = UA + PRM_A*R + PRM_B/R
   
   CALL COMPUTE_NON_LINEAR_TERMS(&
@@ -300,6 +302,8 @@ program tcheby_1d
      SZ = (2._DP*UZ-0.5_DP*UZM1)/DT - DG02 
      SR = (2._DP*UR-0.5_DP*URM1)/DT - DG03
 
+     CALL DEALIAZING(UA,UZ,UR)
+     
      DG10 = UA + PRM_A*R + PRM_B/R
 
      CALL COMPUTE_NON_LINEAR_TERMS(&
@@ -490,6 +494,9 @@ program tcheby_1d
         CALL IMPORT_HDF5(FILENAME,msh,DG04,DG05,DG06)
      end if
 
+     CALL DEALIAZING(UA,UZ,UR)
+     CALL DEALIAZING(DG04,DG05,DG06)
+     
      DG10 = DG04 + PRM_A*R + PRM_B/R
      
      CALL COMPUTE_ADJOINT_NON_LINEAR_TERMS_CYL(&
@@ -536,7 +543,10 @@ program tcheby_1d
         SA = (2._DP*UA-0.5_DP*UAM1)/DT - DG01 - 2._DP*DG04
         SZ = (2._DP*UZ-0.5_DP*UZM1)/DT - DG02 - 2._DP*DG05
         SR = (2._DP*UR-0.5_DP*URM1)/DT - DG03 - 2._DP*DG06
-
+        
+        CALL DEALIAZING(UA,UZ,UR)
+        CALL DEALIAZING(DG04,DG05,DG06)
+        
         DG10 = DG04 + PRM_A*R + PRM_B/R
         
         CALL COMPUTE_ADJOINT_NON_LINEAR_TERMS_CYL(&
